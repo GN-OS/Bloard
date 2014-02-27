6e2701218f4a8eb25e005efd9116e6638696107c
uroboro <uroboro.845@gmail.com>
2014-02-27 04:00:56 -0200

    Update preferences.h
    Now it's a proper header file

--- preferences.h
+++ preferences.h
@@ -1,20 +1,30 @@
+#ifndef EXTRA_H
+#define EXTRA_H
+
 static NSString *const preferencesFilePath = @"/var/mobile/Library/Preferences/com.gnos.bloard.plist";
-#define preferencesChangedNotification "com.gnos.bloard.preferences.changed" 
+static const char *preferencesChangedNotification = "com.gnos.bloard.preferences.changed";
  
-#define addObserver(c, n) CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (c), CFSTR(n), NULL, CFNotificationSuspensionBehaviorCoalesce)
+#define CFNCAddObserver(c, n)   do { CFStringRef cfStr = CFStringCreateWithCString(NULL, n, kCFStringEncodingUTF8); \
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (c), cfStr, NULL, CFNotificationSuspensionBehaviorCoalesce); CFRelease(cfStr); } while (0)
+#define CFNCPostNotification(n) do { CFStringRef cfStr = CFStringCreateWithCString(NULL, n, kCFStringEncodingUTF8); \
+	CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), cfStr, NULL, NULL, YES); CFRelease(cfStr); } while (0)
+
+#define addObserver(c, n) CFNCAddObserver(c, n)
 #define preferencesChanged() preferencesChangedCallback(NULL, NULL, NULL, NULL, NULL)
 
-static NSDictionary *prefs = nil;
+static NSDictionary *preferences = nil;
 
 static void preferencesChangedCallback(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
 	// Reload prefs
-	[prefs release];
-	prefs = [[NSDictionary alloc] initWithContentsOfFile:preferencesFilePath];
+	[preferences release];
+	preferences = [[NSDictionary alloc] initWithContentsOfFile:preferencesFilePath];
 }
 
 static BOOL tweakIsEnabled(void) {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	BOOL enabled = (prefs)? [prefs[@"enabled"] boolValue] : NO;
+	BOOL enabled = [preferences[@"enabled"] boolValue];
 	[pool release];
 	return enabled;
 }
+
+#endif /* EXTRA_H */
